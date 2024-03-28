246a5568b786a94037baedf56b0b08afe9ec6da9
Ben Tuason <benjosedaa@gmail.com>
2024-03-28 14:17:47 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/Home.js";
 import Fuel from "./pages/Fuel.js";
 import Signup from "./pages/Signup.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+//test commit
 const App = () => {
   return (
     <BrowserRouter>
