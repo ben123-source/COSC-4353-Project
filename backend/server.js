2064b486ca382f39d67c1349977bc17f6e812dbc
Ben Tuason <benjosedaa@gmail.com>
2024-03-29 19:43:59 -0400

    changed ports

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.post('/profilepage', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
